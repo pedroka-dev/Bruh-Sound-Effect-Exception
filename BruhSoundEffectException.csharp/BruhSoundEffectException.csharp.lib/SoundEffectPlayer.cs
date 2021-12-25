46b3eec187a686f760c1acea5cf7830c6bfc1789
Pedroca <pedro.henric.rosario@gmail.com>
2021-12-25 17:36:00 -0300

    Makes the bruh sound effect 2 when pressing the button

---
+++ BruhSoundEffectException.csharp/BruhSoundEffectException.csharp.lib/SoundEffectPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruhSoundEffectException.csharp.lib
+{
+    public class SoundEffectPlayer
+    {
+        public static void PlaySound()
+        {
+            string path = @"..\..\..\..\sounds\bruhSoundEffect2.wav";
+            using (var soundPlayer = new SoundPlayer(path))
+            {
+                soundPlayer.Play(); // can also use soundPlayer.PlaySync()
+            }
+        }
+
+    }
+}
